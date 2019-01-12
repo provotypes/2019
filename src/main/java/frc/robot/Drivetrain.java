3821bb3b057a60a154c611553278ad7d549dce86
cabobalot <casey8cockrell@gmail.com>
2019-01-12 11:47:09 -0700

    implemented easypath drivetrain to the drivetrain

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,10 +1,34 @@
 package frc.robot;
 
-public class Drivetrain {
+import easypath.EasyPathDriveTrain;
+
+public class Drivetrain implements EasyPathDriveTrain {
 
     public Drivetrain() {
     
     super();
+}
+
+@Override
+public void setLeftRightDriveSpeed(double left, double right){
+    //TODO fill this
+}
+
+@Override
+public double getInchesTraveled(){
+    //TODO fill this
+    return 0.0d;
+}
+
+@Override
+public double getCurrentAngle(){
+    //TODO fill this
+    return 0.0d;
+}
+
+@Override
+public void resetEncodersAndGyro(){
+    //TODO fill this
 }
 
 }
