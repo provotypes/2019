9ba9877b9cd2de5dfa559bd3f56849a8519103a5
ArviH <44539916+ArviH@users.noreply.github.com>
2019-01-16 19:07:06 -0700

    Update Drivetrain.java

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,26 +1,82 @@
 package frc.robot;
 
-import easypath.EasyPathDriveTrain;
+import com.revrobotics.CANSparkMax;
 
-public class Drivetrain implements EasyPathDriveTrain, DrivetrainInterface {
+import easypath.EasyPathDriveTrain;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Drivetrain extends DifferentialDrive implements EasyPathDriveTrain, DrivetrainInterface {
+
+
+
+static CANSparkMax front_left = new CANSparkMax(1 , MotorType.kBrushless);
+static CANSparkMax rear_left = new CANSparkMax(2 , MotorType.kBrushless);
+
+static SpeedControllerGroup left_a = new SpeedControllerGroup(front_left , rear_left);
+
+static CANSparkMax front_right = new CANSparkMax(3 , MotorType.kBrushless);
+static CANSparkMax rear_right = new CANSparkMax(4 , MotorType.kBrushless);
+
+static SpeedControllerGroup right_a = new SpeedControllerGroup( front_right , rear_right );
+
+ADXRS450_Gyro gyro;
+Encoder encoderLeft, encoderRight;
+
+public static final double TICKS_PER_INCH = 2048 / (6 * Math.PI);
+public static final double DISTANCE_PER_PULSE = 1 / TICKS_PER_INCH;
+gyro = new ADXRS450_Gyro(Port.kOnboardCS0);
+
+encoderLeft = new Encoder(2, 3);
+encoderRight = new Encoder(4, 5);
+
+
 
     public Drivetrain() {
-        super();
-    }
+        super(left_a , right_a);
+
+        encoderLeft.setDistancePerPulse(DISTANCE_PER_PULSE);
+    encoderRight.setDistancePerPulse(DISTANCE_PER_PULSE);
+    public double getLeftEncoderDistance() {
+    return encoderLeft.getDistance();
+    
+}
 
     @Override
     public void setLeftRightDriveSpeed(double left, double right){
-        //TODO fill this
+        tankDrive(left, right);
     }
 
     @Override
     public void setArcadeDriveSpeed(double speed, double turn){
-        //TODO fill this
+        arcadeDrive(speed, turn);
     }
 
-    @Override
+
+
+}
+
+public double getLeftEncoderDistance() {
+		return encoderLeft.getDistance();
+	}
+
+	public double getRightEncoderDistance() {
+		return encoderRight.getDistance();
+
+
+    */
+  
+
+
+
+
+@Override
     public double getInchesTraveled(){
-        //TODO fill this
+
         return 0.0d;
     }
 
