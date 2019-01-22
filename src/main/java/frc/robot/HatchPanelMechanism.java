fa44233288bc2f10743c03ac68a0650d83ccd3f5
ArviH <44539916+ArviH@users.noreply.github.com>
2019-01-21 17:37:23 -0700

    Create HatchPanelMechanism.java

---
+++ src/main/java/frc/robot/HatchPanelMechanism.java
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+
+public class HatchPanelMechanism {
+
+   
+    DoubleSolenoid arm = new DoubleSolenoid(1, 2);
+
+    // This is the method that Brennon said we don't really need. I guess I'll just leave it for now.
+
+    public void solenoidOff(){
+
+    arm.set(DoubleSolenoid.Value.kOff);
+
+    }
+    
+    public void armOut(){    
+        
+    arm.set(DoubleSolenoid.Value.kForward);
+    
+    }
+    
+    //armBack or armIn?
+    
+    public void armBack(){
+    
+    arm.set(DoubleSolenoid.Value.kReverse);
+    
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
