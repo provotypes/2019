4901fdf2b9b6273c7710fdc853efb28021f5bf3a
Marissa Angell <littlemissriss@gmail.com>
2019-01-26 12:06:15 -0700

    Add stubs of Cargo and Hatch mechanism tasks

---
+++ src/main/java/frc/robot/autotasks/HatchMechanismTask.java
@@ -0,0 +1,25 @@
+package frc.robot.autotasks;
+
+public class HatchMechanismTask implements TaskInterface {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end() {
+
+    }
+
+}
