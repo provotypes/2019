6d543043850beb213a9eda6693d2830fda61aba5
cabobalot <casey8cockrell@gmail.com>
2019-01-30 13:43:37 -0700

    clean up and robustify
    (badly)

--- src/main/java/frc/robot/VisionCom.java
+++ src/main/java/frc/robot/VisionCom.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 
 import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class VisionCom {
@@ -19,15 +19,13 @@ public class VisionCom {
 	// java -jar whitetapevision.jar http://10.68.44.2:5800/stream.mjpg
 	// java -jar whitetapevision.jar http://169.254.57.251:5800/stream.mjpg
 	private final String hostName = "10.68.44.11";
-	private final int portNumber = 5801;
+	private final int visionPortNumber = 5801;
 
 	private final int CAMERA_ANGLE = 90;
 
 	private UsbCamera source;
 	private MjpegServer server;
-	private double exposure = 50;
-
-	private double cubeAngle = 0;
+	private int exposure = 50;
 
 	private double width = 390;
 
@@ -35,6 +33,8 @@ public class VisionCom {
 		source = CameraServer.getInstance().startAutomaticCapture();
 		source.setResolution(390, 260);
 		source.setFPS(60);
+		source.setExposureManual(exposure);
+		SmartDashboard.putNumber("exposure", exposure);
 
 		server = CameraServer.getInstance().addServer("VisionCam", 5800);
 		server.setSource(source);
@@ -42,48 +42,21 @@ public class VisionCom {
 	}
 
 	public void updateExposure() {
-		// SmartDashboard.putString("MJPEG URL", server.getListenAddress());
-		try {
-			double brightness = 50;
-			brightness = Integer.parseInt(get(Requests.AVERAGE_BRIGHTNESS));
-//			exposure += 100 - (brightness * 0.1);
-//			exposure = (exposure * 0.95) + ((100 - (brightness) * .5));
-//			exposure =
-			
-			exposure -= (brightness - 50) * 0.01;
-			/*
-			if (brightness > 50) {
-//				exposure -= 1.0d / 50.0d;
-				exposure -= brightness - 50;
-			}
-			else if (brightness < 50) {
-//				exposure += 1.0d / 50.0d;
-				exposure += brightness - 50;
-			}
-			*/
-			
-			if (exposure > 100) {
-				exposure = 100;
-			}
-			if (exposure < 0) {
-				exposure = 0;
-			}
-			SmartDashboard.putNumber("brightness", brightness);
-			source.setExposureManual((int) exposure);
-		} catch (Exception e) {
-			e.printStackTrace();
+		exposure = (int) SmartDashboard.getNumber("exposure", -1);
+		if (exposure < 0){
 			source.setExposureAuto();
+		} else if (exposure > 100){
+			exposure = 100;
+			source.setExposureManual(exposure);
+		}	else {
+			source.setExposureManual(exposure);
 		}
-		SmartDashboard.putNumber("exposure", exposure);
 	}
 
-	public void resetWidth(){
+	public void getStreamWidth() {
 		try {
-			double width = Integer.parseInt(get(Requests.WIDTH));
+			width = Integer.parseInt(get(Requests.WIDTH));
 		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -91,60 +64,46 @@ public class VisionCom {
 
 	public void doStuff() {
 
-		try {
-			String cubeXY = get(Requests.NEAREST_CUBE);
-			double cubeX = Integer.parseInt(cubeXY.substring(0, cubeXY.indexOf(",")));
-			double cubeAngle = ((cubeX * CAMERA_ANGLE) / width) - (CAMERA_ANGLE / 2);
+		updateExposure();
+		
+		String cubeXY = get(Requests.NEAREST_CUBE);
+		double cubeX = Integer.parseInt(cubeXY.substring(0, cubeXY.indexOf(",")));
+		double cubeAngle = ((cubeX * CAMERA_ANGLE) / width) - (CAMERA_ANGLE / 2);
 
-			SmartDashboard.putNumber("line angle", cubeAngle);
-			SmartDashboard.putNumber("cubeX", cubeX);
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		SmartDashboard.putNumber("line angle", cubeAngle);
+		SmartDashboard.putNumber("line X", cubeX);
 
 	}
 
 	public double getAngleToCube() {
 
-		try {
-			SmartDashboard.putString("cube distance", get(Requests.NEAREST_CUBE_DISTANCE));
-
-			double width = Integer.parseInt(get(Requests.WIDTH));
-			String cubeXY = get(Requests.NEAREST_CUBE);
-			double cubeX = Integer.parseInt(cubeXY.substring(0, cubeXY.indexOf(",")));
-			cubeAngle = ((cubeX * CAMERA_ANGLE) / width) - (CAMERA_ANGLE / 2);
-
-			SmartDashboard.putNumber("angle", cubeAngle);
-			SmartDashboard.putNumber("cubeX", cubeX);
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String cubeXY = get(Requests.NEAREST_CUBE);
+		double cubeX = Integer.parseInt(cubeXY.substring(0, cubeXY.indexOf(",")));
+		double cubeAngle = ((cubeX * CAMERA_ANGLE) / width) - (CAMERA_ANGLE / 2);
 
 		return cubeAngle;
 	}
 
-	public String get(int message) throws IOException {
+	public String get(int request) {
 
 		String output = "";
 		// connect
 		try {
-			Socket socket = new Socket(hostName, portNumber);
+			Socket socket = new Socket(hostName, visionPortNumber);
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
 			// send and receive
-			out.println(message);
+			out.println(request);
 			// System.out.println("Server: " + in.readLine());
 			output = in.readLine();
 
 			socket.close();
 
 		} catch (ConnectException e) {
-
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return output;
 
