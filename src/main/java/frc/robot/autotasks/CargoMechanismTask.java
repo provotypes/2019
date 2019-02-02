59cfdaa89e474d59b3f63cf1062405902bab9959
Marissa Angell <littlemissriss@gmail.com>
2019-02-01 19:12:50 -0700

    Filled out CargoMechanismTask

--- src/main/java/frc/robot/autotasks/CargoMechanismTask.java
+++ src/main/java/frc/robot/autotasks/CargoMechanismTask.java
@@ -1,15 +1,39 @@
 package frc.robot.autotasks;
 
+import frc.robot.CargoMechanismInterface;
+
 public class CargoMechanismTask implements TaskInterface {
+
+    private CargoMechanismInterface mechanism;
+    private boolean isShooting;
+    private int numTicks;
+    private int duration;
+
+    //TODO: make boolean isShooting into enum typeOfTask
+    //TODO: make unit of duration variable seconds instead of ticks
+
+    public CargoMechanismTask(CargoMechanismInterface c, boolean isShooting, int duration){
+        this.mechanism = c;
+        this.isShooting = isShooting;
+        this.duration = duration;
+    }
 
     @Override
     public void start() {
-
+        numTicks = 0;
     }
 
     @Override
     public void execute() {
 
+        while (numTicks < duration){
+            if (isShooting){
+                mechanism.launchBall();
+            } else {
+                mechanism.intakeBall();
+            }
+        }
+        numTicks++;
     }
 
     @Override
@@ -19,7 +43,11 @@ public class CargoMechanismTask implements TaskInterface {
 
     @Override
     public void end() {
-
+        if (isShooting){
+            mechanism.launchBallOff();
+        } else {
+            mechanism.intakeBallOff();
+        }
     }
 
 }
