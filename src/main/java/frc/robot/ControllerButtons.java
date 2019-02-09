c3672feff2dfbdb9f04c5bb1ddb471b1362e16a3
ArviH <44539916+ArviH@users.noreply.github.com>
2019-02-09 12:26:17 -0700

    Create ControllerButtons.java

---
+++ src/main/java/frc/robot/ControllerButtons.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class ControllerButtons {
+    
+    public static final int panelFloorPickup = 6;
+
+    public static final int panelDetach = 5;
+
+    public static final int pannelStow = 3;
+
+    public static final int cargoIntake = 2;
+
+    public static final int cargoLaunch = 1;
+
+    public static final int cargoReverse = 4;
+
+    public static final int autoSwitch = 8;
+
+
+}
