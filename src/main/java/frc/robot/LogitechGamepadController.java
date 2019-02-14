b66d54dbb2a9fcd8392906d810bd74ec652eac32
Brennon Brimhall <brennonbrimhall@provotypes.org>
2019-02-14 16:30:37 -0700

    Make button constants static.

--- src/main/java/frc/robot/LogitechGamepadController.java
+++ src/main/java/frc/robot/LogitechGamepadController.java
@@ -14,22 +14,22 @@ package frc.robot;
 public class LogitechGamepadController extends ProvotypesJoystick {
 
 	//Buttons
-	public final int A_BUTTON = 1;
-	public final int B_BUTTON = 2;
-	public final int X_BUTTON = 3;
-	public final int Y_BUTTON = 4;
-	public final int LEFT_BUMPER = 5;
-	public final int RIGHT_BUMPER = 6;
-	public final int START_BUTTON = 8;
-	public final int LEFT_STICK_IN = 9;
+	public static final int A_BUTTON = 1;
+	public static final int B_BUTTON = 2;
+	public static final int X_BUTTON = 3;
+	public static final int Y_BUTTON = 4;
+	public static final int LEFT_BUMPER = 5;
+	public static final int RIGHT_BUMPER = 6;
+	public static final int START_BUTTON = 8;
+	public static final int LEFT_STICK_IN = 9;
 
 	//Axes
-	public final int LEFT_X_AXIS = 0;
-	public final int LEFT_Y_AXIS = 1;
-	public final int LEFT_TRIGGER = 2;
-	public final int RIGHT_X_AXIS = 4;
-	public final int RIGHT_Y_AXIS = 5;
-	public final int RIGHT_TRIGGER = 3;
+	public static final int LEFT_X_AXIS = 0;
+	public static final int LEFT_Y_AXIS = 1;
+	public static final int LEFT_TRIGGER = 2;
+	public static final int RIGHT_X_AXIS = 4;
+	public static final int RIGHT_Y_AXIS = 5;
+	public static final int RIGHT_TRIGGER = 3;
 
 	public LogitechGamepadController(int pos) {
 		super(pos);
