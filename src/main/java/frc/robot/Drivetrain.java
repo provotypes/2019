11ea088a2b54f4ed17e8ffbf03823d0ca83024cb
Brennon Brimhall <brennonbrimhall@provotypes.org>
2019-02-13 22:26:26 -0700

    Prevent robot from tipping.
    Will likely need to adjust constant down; need to test on carpet.

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -5,94 +5,88 @@ import com.revrobotics.CANSparkMaxLowLevel;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
-// import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-//-------------------------------------------------------------------------------------------
-
 public class Drivetrain extends DifferentialDrive implements DrivetrainInterface {
+	public static final double TICKS_PER_INCH = 2048.0d / (6.0d * Math.PI); //FIXME calibrate this
+	public static final double DISTANCE_PER_PULSE = 1.0d / TICKS_PER_INCH;
+	public static final double RAMP_PERIOD = 0.5;
 
-    // /* spark max (neo motors)
-                                                        // there are like a million MotorType objects
-    static CANSparkMax front_left = new CANSparkMax(1 , CANSparkMaxLowLevel.MotorType.kBrushless);
-    static CANSparkMax rear_left = new CANSparkMax(2 , CANSparkMaxLowLevel.MotorType.kBrushless);
+	static CANSparkMax front_left = new CANSparkMax(1 , CANSparkMaxLowLevel.MotorType.kBrushless);
+	static CANSparkMax rear_left = new CANSparkMax(2 , CANSparkMaxLowLevel.MotorType.kBrushless);
 
-    static SpeedControllerGroup left_a = new SpeedControllerGroup(front_left , rear_left);
+	static SpeedControllerGroup left_a = new SpeedControllerGroup(front_left, rear_left);
 
-    static CANSparkMax front_right = new CANSparkMax(3 , CANSparkMaxLowLevel.MotorType.kBrushless);
-    static CANSparkMax rear_right = new CANSparkMax(4 , CANSparkMaxLowLevel.MotorType.kBrushless);
+	static CANSparkMax front_right = new CANSparkMax(3 , CANSparkMaxLowLevel.MotorType.kBrushless);
+	static CANSparkMax rear_right = new CANSparkMax(4 , CANSparkMaxLowLevel.MotorType.kBrushless);
 
-    static SpeedControllerGroup right_a = new SpeedControllerGroup(front_right , rear_right);
-    // */
+	static SpeedControllerGroup right_a = new SpeedControllerGroup(front_right, rear_right);
 
-    // static SpeedControllerGroup left_a = new SpeedControllerGroup(new Spark(0) , new Spark(1));
-    // static SpeedControllerGroup right_a = new SpeedControllerGroup(new Spark(2) , new Spark(3));
+	ADXRS450_Gyro gyro;
+	
+	Encoder encoderLeft, encoderRight;
 
+	public Drivetrain() {
+		super(left_a, right_a);
 
-    ADXRS450_Gyro gyro;
-    
-    Encoder encoderLeft, encoderRight;
+		front_left.setRampRate(RAMP_PERIOD);
+		rear_left.setRampRate(RAMP_PERIOD);
 
-    public static final double TICKS_PER_INCH = 2048.0d / (6.0d * Math.PI); //FIXME calibrate this
-    public static final double DISTANCE_PER_PULSE = 1.0d / TICKS_PER_INCH;
+		front_right.setRampRate(RAMP_PERIOD);
+		rear_right.setRampRate(RAMP_PERIOD);
 
-    public Drivetrain() {
-        
-        super(left_a , right_a);
-        
-        left_a.setInverted(true);
-        
-        right_a.setInverted(true);
-        
-        gyro = new ADXRS450_Gyro();
+		left_a.setInverted(true);
+		right_a.setInverted(true);
+		
+		gyro = new ADXRS450_Gyro();
 
-        encoderLeft = new Encoder(2, 3);
-        encoderRight = new Encoder(1, 0);
+		encoderLeft = new Encoder(2, 3);
+		encoderRight = new Encoder(1, 0);
 
-        encoderLeft.setDistancePerPulse(DISTANCE_PER_PULSE);
-        encoderRight.setDistancePerPulse(DISTANCE_PER_PULSE);
-    }
+		encoderLeft.setDistancePerPulse(DISTANCE_PER_PULSE);
+		encoderRight.setDistancePerPulse(DISTANCE_PER_PULSE);
+	}
 
-    @Override
-    public void setLeftRightDriveSpeed(double left, double right){
-        tankDrive(left, right);
-    }
+	@Override
+	public void setLeftRightDriveSpeed(double left, double right){
+		tankDrive(left, right);
+	}
 
-    @Override
-    public void setArcadeDriveSpeed(double speed, double turn){
-        arcadeDrive(speed, turn);
-    }
+	@Override
+	public void setArcadeDriveSpeed(double speed, double turn){
+		arcadeDrive(speed, turn);
+	}
 
-    @Override
-    public double getInchesTraveled(){
-        // average
-        return ((getLeftEncoderDistance() + getRightEncoderDistance()) / 2.0d);
-    }
+	@Override
+	public double getInchesTraveled(){
+		// average
+		return ((getLeftEncoderDistance() + getRightEncoderDistance()) / 2.0d);
+	}
 
-    @Override
-    public double getCurrentAngle(){
-        return gyro.getAngle();
-    }
+	@Override
+	public double getCurrentAngle(){
+		return gyro.getAngle();
+	}
 
-    @Override
-    public void resetEncodersAndGyro(){
-        encoderLeft.reset();
-        encoderRight.reset();
-        gyro.reset();
-    }
+	@Override
+	public void resetEncodersAndGyro(){
+		encoderLeft.reset();
+		encoderRight.reset();
+		gyro.reset();
+	}
 
-    public double getLeftEncoderDistance() {
+	public double getLeftEncoderDistance() {
 		return encoderLeft.getDistance();
 	}
 
 	public double getRightEncoderDistance() {
 		return encoderRight.getDistance();
-    }
+	}
 
-    @Override
-    public void calibrateGyro(){
-        gyro.calibrate();
-    }
+	@Override
+	public void calibrateGyro(){
+		gyro.calibrate();
+	}
 
 }
