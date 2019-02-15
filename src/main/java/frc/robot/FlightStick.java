c3a59bff9babc628e615dd6d593ea6b70af3b1d2
cabobalot <casey8cockrell@gmail.com>
2019-02-14 19:34:18 -0700

    Added

---
+++ src/main/java/frc/robot/FlightStick.java
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Bindable Joystic Class for the flightstick.
+ */
+public class FlightStick extends BindableJoystick {
+	public FlightStick(int port) {
+		super(port);
+	}
+}
