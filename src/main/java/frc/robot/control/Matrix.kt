4494e27d22f9e1974f66a0cb40c51d32c06ac3af
Brennon Brimhall <brennonbrimhall@byu.edu>
2019-05-17 11:14:45 -0600

    Add documentation.

--- src/main/java/frc/robot/control/Matrix.kt
+++ src/main/java/frc/robot/control/Matrix.kt
@@ -1,6 +1,22 @@
 package frc.robot.control
 
+/**
+ * A class to represent arbitrary matrices of doubles.  Primarily used to
+ * facilitate state space control.
+ *
+ * @author Brennon Brimhall <brennonbrimhall@provotypes.org
+ * @property height the number of rows
+ * @property width the number of cols
+ * @param initFunction the function used to initialize the matrix
+ * @property indices the valid one-dimensional indices of this matrix
+ * @property rows the valid rows of this matrix
+ * @property cols the valid cols of this matix
+ */
 class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { i -> 0.0 }) {
+
+    /**
+     * The array used to store the data.
+     */
     private var data: DoubleArray = DoubleArray(height * width, initFunction)
 
     val indices: IntRange
@@ -12,6 +28,9 @@ class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { 
     val cols: IntRange
         get() = IntRange(0, width - 1)
 
+    /**
+     * Retrieve the element at the given two-dimensional location in the matrix.
+     */
     operator fun get(row: Int, col: Int): Double {
         if (row >= height || col >= width) {
             throw IndexOutOfBoundsException()
@@ -20,10 +39,17 @@ class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { 
         return data[col + (row * width)]
     }
 
+    /**
+     * Retrieve the element at the given one-dimensional location in the matrix.
+     */
     operator fun get(i: Int): Double {
         return data[i]
     }
 
+    /**
+     * Set the element at the given two-dimensional location in the matrix to
+     * the specified value.
+     */
     operator fun set(row: Int, col: Int, d: Double) {
         if (row >= height || col >= width) {
             throw IndexOutOfBoundsException()
@@ -32,43 +58,18 @@ class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { 
         data[col + (row * width)] = d
     }
 
+    /**
+     * Set the element at the given one-dimensional location in the matrix to
+     * the specified value.
+     */
     operator fun set(i: Int, d: Double) {
         data[i] = d
     }
 
-    private fun sameSize(m: Matrix): Boolean {
-        return m.width == width && m.height == height
-    }
-
-    fun elementwise(f: (Double) -> Double): Matrix {
-        return Matrix(height, width) { i -> f(this[i]) }
-    }
-
-    fun applyElementwise(f: (Double) -> Double) {
-        for (i in data.indices) {
-            this[i] = f(this[i])
-        }
-    }
-
-    fun elementwise(m: Matrix, f: (Double, Double) -> Double): Matrix {
-        return Matrix(height, width) { i -> f(this[i], m[i]) }
-    }
-
-
-    fun applyElementwise(m: Matrix, f: (Double, Double) -> Double) {
-        if (!sameSize(m)) {
-            throw IndexOutOfBoundsException()
-        }
-
-        for (i in indices) {
-            this[i] = f(this[i], m[i])
-        }
-    }
-
-    fun copy(): Matrix {
-        return elementwise { d -> d }
-    }
-
+    /**
+     * Perform matrix addition.  Throws an error if the provided matrix is not
+     * the same size as this matrix.
+     */
     operator fun plus(m: Matrix): Matrix {
         if (!sameSize(m)) {
             throw IndexOutOfBoundsException()
@@ -77,6 +78,10 @@ class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { 
         return elementwise(m) { a, b -> a + b }
     }
 
+    /**
+     * Perform matrix subtraction.  Throws an error if the provided matrix is
+     * not the same size as this matrix.
+     */
     operator fun minus(m: Matrix): Matrix {
         if (!sameSize(m)) {
             throw IndexOutOfBoundsException()
@@ -85,6 +90,10 @@ class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { 
         return elementwise(m) { a, b -> a - b }
     }
 
+    /**
+     * Perform matrix multiplication.  Throws an error if the provided matrix's
+     * height does not equal this matrix's width.
+     */
     operator fun times(m: Matrix): Matrix {
         if (this.width != m.height) {
             throw IndexOutOfBoundsException()
@@ -107,20 +116,9 @@ class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { 
         return result
     }
 
-    fun clip(lower: Matrix, upper: Matrix) {
-        for (i in indices) {
-            if (this[i] > upper[i]) {
-                this[i] = upper[i]
-            } else if (this[i] < lower[i]) {
-                this[i] = lower[i]
-            }
-        }
-    }
-
-    override fun hashCode(): Int {
-        return data.hashCode()
-    }
-
+    /**
+     * Compare this matrix to another object and determine if they are equal.
+     */
     override operator fun equals(other: Any?): Boolean {
         if (other !is Matrix) {
             return false
@@ -131,5 +129,88 @@ class Matrix(val height: Int, val width: Int, initFunction: (Int) -> Double = { 
         }
 
         return data.contentEquals(other.data)
+    }
+
+    /**
+     * Compare the dimensions of another matrix and determine if they are
+     * equal to our dimensions.
+     */
+    private fun sameSize(m: Matrix): Boolean {
+        return m.width == width && m.height == height
+    }
+
+    /**
+     * Provide a hash code for the JVM.
+     */
+    override fun hashCode(): Int {
+        return data.hashCode()
+    }
+
+    /**
+     * Compute a new matrix by using the provided function to calculate each
+     * element.
+     */
+    fun elementwise(f: (Double) -> Double): Matrix {
+        return Matrix(height, width) { i -> f(this[i]) }
+    }
+
+    /**
+     * Apply the given function in place to each element of this matrix.
+     */
+    fun applyElementwise(f: (Double) -> Double) {
+        for (i in data.indices) {
+            this[i] = f(this[i])
+        }
+    }
+
+    /**
+     * Compute a new matrix by using the provided secondary matrix and function
+     * to calculate each element.
+     */
+    fun elementwise(m: Matrix, f: (Double, Double) -> Double): Matrix {
+        return Matrix(height, width) { i -> f(this[i], m[i]) }
+    }
+
+    /**
+     * Apply the given function in place to each element of this matrix, using
+     * the secondary matrix.
+     */
+    fun applyElementwise(m: Matrix, f: (Double, Double) -> Double) {
+        if (!sameSize(m)) {
+            throw IndexOutOfBoundsException()
+        }
+
+        for (i in indices) {
+            this[i] = f(this[i], m[i])
+        }
+    }
+
+    /**
+     * Copy this matrix's data into a new matrix.
+     */
+    fun copy(): Matrix {
+        return elementwise { d -> d }
+    }
+
+    /**
+     * Clip the elements of this matrix so that the ith element of this matrix
+     * is between the ith element of min and the ith element of max, inclusive.
+     */
+    fun clip(lower: Matrix, upper: Matrix) {
+        if (!sameSize(lower)) {
+            throw IndexOutOfBoundsException()
+        }
+
+        if (!sameSize(upper)) {
+            throw IndexOutOfBoundsException()
+        }
+
+        for (i in indices) {
+            if (this[i] > upper[i]) {
+                this[i] = upper[i]
+            } else if (this[i] < lower[i]) {
+                this[i] = lower[i]
+            }
+        }
     }
 }
